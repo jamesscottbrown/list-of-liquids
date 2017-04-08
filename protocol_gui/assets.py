94d016fca2452f48fa75a144149580adf25ed525
James Scott-Brown <james@jamesscottbrown.com>
2017-04-08 15:13:20 +0100

    Modify dcript/css includes

--- protocol_gui/assets.py
+++ protocol_gui/assets.py
@@ -9,6 +9,10 @@ css = Bundle(
     output='public/css/common.css'
 )
 
+css_network_editor = Bundle(
+	'css/network_editor.css'
+)
+
 js = Bundle(
     'libs/jQuery/dist/jquery.js',
     'libs/bootstrap/dist/js/bootstrap.js',
@@ -17,7 +21,15 @@ js = Bundle(
     output='public/js/common.js'
 )
 
+js_network_editor = Bundle(
+	'js/network_editor.js',
+	'libs/cola.v3.min.js',
+	'libs/d3.v3.min.js'
+)
+
 assets = Environment()
 
 assets.register('js_all', js)
+assets.register('js_network_editor', js_network_editor)
 assets.register('css_all', css)
+assets.register('css_network_editor', css_network_editor)
