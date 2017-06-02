jamesscottbrown/list-of-liquids

commit 68c7ce7a4dbc5982d500178fb8542275b316be33

Add ability to specify pipette in panel

protocol_gui/static/js/network_editor.js
@@ -69,6 +69,15 @@ function network_editor() {
     }
     lastNodeId = nodes.length - 1;
 
+    var default_link_data = {
+        volumes: [1],
+        addToThis: true,
+        changeTips: false,
+        mix: false,
+        num_duplicates: 1,
+        pipette_name: ''
+    };
+
     var force = cola.d3adaptor()
         .linkDistance(50)
         .size([width, height])
@@ -737,7 +746,7 @@ function network_editor() {
             links.push({
                 source: mousedown_node,
                 target: mouseup_node,
-                data: {volumes: [1], addToThis: true, changeTips: false, mix: false, num_duplicates: 1}
+                data: default_link_data
             });
             selected_node = mouseup_node;
             return;
@@ -754,12 +763,12 @@ function network_editor() {
         links.push({
             source: mousedown_node,
             target: nodes[i],
-            data: {volumes: [1], addToThis: false, changeTips: false, mix: false, num_duplicates: 1}
+            data: default_link_data
         });
         links.push({
             source: mouseup_node,
             target: nodes[i],
-            data: {volumes: [1], addToThis: true, changeTips: false, mix: false, num_duplicates: 1}
+            data: default_link_data
         });
 
         selected_link = null;
@@ -809,7 +818,7 @@ function network_editor() {
         i = addProcessNode(kind);
         links.push({
             source: sourceNode, target: nodes[i],
-            data: {volumes: [1], addToThis: null, changeTips: null, mix: null, num_duplicates: 1}
+            data: default_link_data
         });
         restart();
     }

protocol_gui/static/js/equipment.js
@@ -51,6 +51,7 @@ function update_container_list() {
                 };
                 $('#containerModal').modal('toggle');
                 update_container_list();
+                updateDescriptionPanel();
             });
         });
 }
@@ -91,12 +92,25 @@ function update_pipette_list() {
             d3.select("#pipetteVolume").property('value', d.volume);
 
             d3.select("#AddPipetteButton").on("click", function () {
+
+                var oldName = d.name;
+                var newName = d3.select("#pipetteName").node().value;
+
+                // adjust any links to match the new pipette name
+                for (var j = 0; j < links.length; j++) {
+                    if (links[j].data.pipette_name == oldName) {
+                        links[j].data.pipette_name = newName;
+                    }
+                }
+
                 pipettes[i] = {
-                    name: d3.select("#pipetteName").node().value,
+                    name: newName,
                     volume: d3.select("#pipetteVolume").node().value
                 };
                 $('#pipetteModal').modal('toggle');
                 update_pipette_list();
+                updateDescriptionPanel();
+
             });
         });
 

protocol_gui/static/js/panel.js
@@ -274,6 +274,41 @@ function drawTransferPanel(selected_node, selected_link, links, restart, redrawL
     containerSelect.node().value = (selected_link.data.addToThis ? "Yes" : "No");
 
 
+    // Form to specify which pipette to use
+    var pipetteDiv = form.append("div").append("div")
+        .classed("form-group", true);
+
+    pipetteDiv.append("label")
+        .classed("control-label", true)
+        .classed("col-sm-5", true)
+        .attr("for", "pipette")
+        .text("Pipette: ");
+
+    var pipetteInput = pipetteDiv
+        .append("select")
+        .classed("col-sm-5", true)
+        .classed("control-input", true)
+        .attr("name", "pipette")
+        .attr("value", selected_link.data.pipette_name)
+        .on("change", function () {
+            selected_link.data.pipette_name = this.value;
+            restart();
+        });
+
+    pipetteInput.selectAll("option").data(pipettes)
+        .enter()
+        .append("option")
+        .attr("id", function (d) {
+            return d.name;
+        })
+        .text(function (d) {
+            return d.name;
+        });
+
+    // N.B. need to add the options to the select before the value can be set
+    pipetteInput.property("value", selected_link.data.pipette_name);
+
+
     // Form to set whether we are changing tips
     var changeTipDiv = form.append("div").append("div")
         .classed("form-group", true);

commit 2dff36c436b0d3b0d221b595ebbad12a43a6953c

Fix bug introduced in 68c7ce7a4d about default link data

protocol_gui/static/js/network_editor.js
@@ -69,14 +69,19 @@ function network_editor() {
     }
     lastNodeId = nodes.length - 1;
 
-    var default_link_data = {
-        volumes: [1],
-        addToThis: true,
-        changeTips: false,
-        mix: false,
-        num_duplicates: 1,
-        pipette_name: ''
-    };
+    function getDefaultLinkData(addToThis){
+        var default_link_data = {
+            volumes: [1],
+            addToThis: true,
+            changeTips: false,
+            mix: false,
+            num_duplicates: 1,
+            pipette_name: ''
+        };
+
+        default_link_data.addToThis = addToThis;
+        return default_link_data;
+    }
 
     var force = cola.d3adaptor()
         .linkDistance(50)
@@ -746,7 +751,7 @@ function network_editor() {
             links.push({
                 source: mousedown_node,
                 target: mouseup_node,
-                data: default_link_data
+                data: getDefaultLinkData(true)
             });
             selected_node = mouseup_node;
             return;
@@ -763,12 +768,12 @@ function network_editor() {
         links.push({
             source: mousedown_node,
             target: nodes[i],
-            data: default_link_data
+            data:  getDefaultLinkData(true)
         });
         links.push({
             source: mouseup_node,
             target: nodes[i],
-            data: default_link_data
+            data:  getDefaultLinkData(false)
         });
 
         selected_link = null;
@@ -818,7 +823,7 @@ function network_editor() {
         i = addProcessNode(kind);
         links.push({
             source: sourceNode, target: nodes[i],
-            data: default_link_data
+            data:  getDefaultLinkData(true)
         });
         restart();
     }