d0d13029f5e7f7be62cb2968e56e0025ef635a79
James Scott-Brown <james@jamesscottbrown.com>
2017-07-06 14:16:14 +0100

    Add drag-and-drop assignment to wells

--- protocol_gui/static/js/well_assignment.js
+++ protocol_gui/static/js/well_assignment.js
@@ -1,12 +1,14 @@
+var selected_container;
+
 function populationWellAssignmentModal(container_name, serialiseDiagram) {
 
-    var container = containers.filter(function (d) {
+    selected_container = containers.filter(function (d) {
         return d.name == container_name
     })[0];
 
     d3.select("#locationModal").select(".modal-title").text("Well assignments for " + container_name);
 
-    drawContainerDiagram(container);
+    drawContainerDiagram(selected_container);
     listContentsOfContainer(container_name, serialiseDiagram);
 }
 
@@ -37,29 +39,52 @@ function listContainerContents(result, div, queryNode) {
     //d3.select("#node-" + queryNode.id).remove();
     var newDiv = div.append("div").attr("id", "node-" + queryNode.id);
 
+    var data = [];
+    for (var i = 0; i < result.length; i++) {
+        data.push({contents: result[i], aliquot_index: i, operation_index: queryNode.id});
+    }
 
     newDiv.append("h4").text("Node " + queryNode.id);
 
     var outer_list_items = div
         .append("ol")
         .selectAll("li")
-        .classed("well-contents", true)
-        .data(result)
+        .data(data)
         .enter()
         .append("li").style("margin-top", "10px")
-        .append("ul");
+        .classed("well-contents", true)
+
+        .append("ul")
+
+        .attr("draggable", true)
+        .on("dragstart", function (d, i) {
+            var ev = d3.event;
+            ev.dataTransfer.setData("custom-data", queryNode.id + "," + i);
+        })
+        .on("drop", function (a, b, c) {
+            console.log("Dropped");
+        })
+        .on("mouseover", function (d) {
+
+            if (getLocation(d.operation_index, d.aliquot_index)) {
+                highlightWell(d)
+
+            }
+        })
+        .on("mouseout", resetAppearances)
+        ;
 
     var items = outer_list_items.selectAll("li")
         .data(function (d) {
-            return d
+            return d.contents
         })
         .enter()
         .append("li")
         .text(function (d) {
             return d;
         });
+    resetAppearances();
 }
-
 
 function drawContainerDiagram(container) {
 
@@ -135,7 +160,10 @@ function drawContainer(container_data, container) {
 
     d3.select("#locationModal").select("#well-diagram").selectAll("svg").remove();
     var svg = d3.select("#locationModal").select("#well-diagram")
-        .append("svg").attr("width", width).attr("height", height);
+        .append("svg").attr("width", width).attr("height", height)
+        .on("dragover", function (d) {
+            d3.event.preventDefault();
+        });
 
     var xScale = d3.scale.linear().range([padding, width - padding])
         .domain(xExtent);
@@ -169,14 +197,43 @@ function drawContainer(container_data, container) {
         .style("stroke", "black")
         .style("stroke-width", "2px")
 
-        .style("fill", function (d) {
-            return d.contents ? "black" : "white";
-        })
         .on("mouseover", function (d) {
-            if (d.contents) {
-                highlightWell(d.contents)
+            var contents = selected_container.contents[d.name];
+            if (contents) {
+                highlightWell(contents);
             }
-        });
+        })
+        .on("mouseout", resetAppearances)
+        .on("drop", function (d) {
+
+            if (d.container.contents[d.name]) {
+                return;
+            } // ensure well is empty
+
+            var data = d3.event.dataTransfer.getData("custom-data").split(","); // add well
+            var operation_index = data[0];
+            var aliquot_index = data[1];
+
+            // if already placed somewhere else, remove from there first
+            var oldLocation = getLocation(operation_index, aliquot_index);
+            if (oldLocation) {
+                delete selected_container.contents[oldLocation];
+            }
+
+            d.container.contents[d.name] = {operation_index: operation_index, aliquot_index: aliquot_index};
+
+            resetAppearances();
+        })
+        .on("contextmenu", d3.contextMenu(function (d) {
+            return [{
+                title: 'Clear',
+                disabled: !d.contents,
+                action: function (elm, d) {
+                    delete selected_container.contents[d.name];
+                    resetAppearances();
+                }
+            }]
+        }));
 
 
     g.selectAll("rect")
@@ -201,7 +258,8 @@ function drawContainer(container_data, container) {
         .attr("title", function (d) {
             return d.name;
         })
-        .style("fill", "red");
+        .style("stroke", "black")
+        .style("stroke-width", "2px");
 
 
     var g_columns = svg.append("g").attr("transform", "translate(" + container_data["origin-offset"].x + ", 0)");
@@ -234,22 +292,62 @@ function drawContainer(container_data, container) {
             return d.text;
         })
         .style("font-size", "5pt");
+
+    resetAppearances();
+}
+
+function resetAppearances() {
+    // adjusts appearances of wells/aliquots to show whether they are full/have been assigned positions
+
+    d3.selectAll("circle")
+        .style("fill", function (d) {
+            return selected_container.contents[d.name] ? "black" : "white";
+        });
+
+
+    d3.selectAll("rect")
+        .style("fill", function (d) {
+            return selected_container.contents[d.name] ? "black" : "white";
+        });
+
+    d3.selectAll(".well-contents").style("color", function (d) {
+            return getLocation(d.operation_index, d.aliquot_index) ? 'grey' : 'black';
+        })
+        .style("border-left", "none");
+}
+
+
+function getLocation(operation_index, aliquot_index) {
+
+    for (var well in selected_container.contents) {
+        var c = selected_container.contents[well];
+
+        if (c.operation_index == operation_index && c.aliquot_index == aliquot_index) {
+            return well;
+        }
+    }
+    return "";
 }
 
 
 function highlightWell(contents) {
     // Highlight circle
+
+    var wellName = getLocation(contents.operation_index, contents.aliquot_index);
+
     d3.select("#locationModal").selectAll("circle")
-        .style("fill", function (d) {
-            return (d.contents == contents) ? "yellow" : "white";
-        });
+        .filter(function (d) {
+            return d.name == wellName;
+        })
+        .style("fill", "yellow");
 
     // highlight content list
     d3.select("#locationModal")
-        .select(".id", "node-" + contents.operation_index) // get right set of aliauots
-        .select(".well-contents")
-        .style("text-color", function (d, i) {
-            return (i == contents.aliquot_index) ? "yellow" : "black";
+        //.select(".id", "node-" + contents.operation_index) // get right set of aliauots
+        .selectAll(".well-contents")
+        .filter(function (d) {
+            return d.aliquot_index == contents.aliquot_index && d.operation_index == contents.operation_index;
         })
+        .style("border-left", "5px solid yellow");
 }
 
