7ebc129eee04f8e0eb22abb0c8e6f1386ea72042
James Scott-Brown <james@jamesscottbrown.com>
2017-07-22 17:29:56 +0100

    Round volumes to 2 d.p. when listing contents

--- protocol_gui/protocol/views.py
+++ protocol_gui/protocol/views.py
@@ -225,6 +225,6 @@ def get_contents(protocol_id):
     protocol_obj = json.loads(request.args.get("protocol_string"))
 
     result = process_node(protocol_obj, node_id)
-    return json.dumps( map(lambda x: map(lambda y: str(y.volume) + " of " + y.resource, x), result) )
+    return json.dumps( map(lambda x: map(lambda y: '{0:.2f}'.format(y.volume) + " of " + y.resource, x), result) )
 
 
