7a1b2aa2433fe9b6ccc802b3f8d30b91f7a58b6e
James Scott-Brown <james@jamesscottbrown.com>
2017-11-02 21:54:47 +0000

    When operation type changed, update all nodes representing it

--- protocol_gui/static/js/panel.js
+++ protocol_gui/static/js/panel.js
@@ -776,7 +776,13 @@ function drawProcessPanel(selected_node, restart, form, deleteNode) {
             // delete data from node
             data.options = {};
 
-            // update drawing to reflect change to operations (really only need to redraGroups)
+            // update other nodes in same group
+            var operation = operations.filter(function(o){return o.leaves.indexOf(selected_node) != -1; })[0];
+            for (var i=0; i<operation.leaves.length; i++){
+                operation.leaves[i].data.process_type = this.value;
+            }
+
+            // update drawing to reflect change to operations (really only need to redrawGroups)
             restart();
 
             selected_node.data.process_type = this.value;
